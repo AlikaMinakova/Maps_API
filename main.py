49133ea523a55cdcb2140e0a2ef178efbd3bf508
AlinaMinakova <94383135+AlinaMinackova@users.noreply.github.com>
2022-02-02 09:40:51 +0300

    Task_9/10

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Map:
             file.write(response.content)
 
     def mach_coor(self, zoom, posi=None, mash="map", place=''):
-        global found, pt, begin, place2
+        global found, pt, begin, place2, count
         if begin:
             toponym_to_find = "Москва"
             geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
@@ -79,10 +79,24 @@ class Map:
             toponym_coodrinates = toponym["Point"]["pos"]
             ad = toponym["metaDataProperty"]["GeocoderMetaData"]["text"].split(',')
             ad2 = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"][2]["name"]
-            place2 = f"{ad[0]}, {ad2}, {', '.join(ad[1:])}"
+            try:
+                self.postal_code = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+            except KeyError:
+                self.postal_code = '      '
+            count = 2
+            if PC:
+                place2 = f"{ad[0]}, {ad2}, {', '.join(ad[1:])} {self.postal_code}"
+            else:
+                place2 = f"{ad[0]}, {ad2}, {', '.join(ad[1:])}"
             self.toponym_longitude, self.toponym_lattitude = toponym_coodrinates.split(" ")
             found = False
             pt = "{0},pm2dgl".format(",".join([self.toponym_longitude, self.toponym_lattitude]))
+        if PC:
+            if place2 != '' and self.postal_code not in place2:
+                place2 += ' ' + str(self.postal_code)
+        if not PC:
+            if place2 != '' and self.postal_code in place2:
+                place2 = place2[:-6]
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         if pos == 'left' and float(self.toponym_longitude) > -180:
             self.toponym_longitude = str(float(self.toponym_longitude) - 0.0005)
@@ -115,11 +129,13 @@ pt = ''
 begin = False
 found = False
 need_input = False
+PC = False
+count = 0
 pygame.init()
 input_rect = pygame.Rect(8, 400, 300, 35)
 border_rect = pygame.Rect(7, 399, 302, 37)
-input_rect2 = pygame.Rect(65, 6, 500, 25)
-border_rect2 = pygame.Rect(64, 5, 502, 27)
+input_rect2 = pygame.Rect(8, 6, 580, 25)
+border_rect2 = pygame.Rect(7, 5, 582, 27)
 color = [(107, 142, 35), 'gray', 'gray']
 pygame.font.init()
 mash = "map"
@@ -145,13 +161,20 @@ while running:
                 place = ''
                 place2 = ''
                 begin = True
-            if 8 <= x <= 43 and 8 <= y <= 28:
+            if 461 <= x <= 541 and 400 <= y <= 435:
+                if not PC:
+                    PC = True
+                else:
+                    PC = False
+            if 518 <= x <= 578 and 400 <= y <= 435:
+                PC = False
+            if 8 <= x <= 58 and 50 <= y <= 70:
                 mash = "map"
                 color[0], color[1], color[2] = (107, 142, 35), 'gray', 'gray'
-            elif 8 <= x <= 43 and 55 <= y <= 75:
+            elif 8 <= x <= 58 and 90 <= y <= 110:
                 mash = "sat"
                 color[0], color[1], color[2] = 'gray', (107, 142, 35), 'gray'
-            elif 8 <= x <= 43 and 102 <= y <= 122:
+            elif 8 <= x <= 58 and 130 <= y <= 150:
                 mash = "sat,skl"
                 color[0], color[1], color[2] = 'gray', 'gray', (107, 142, 35)
         if event.type == pygame.KEYDOWN:
@@ -180,18 +203,18 @@ while running:
         screen.blit(pygame.image.load(mp.mach_coor(zoom, posi=pos, mash=mash)), (0, 0))
     else:
         screen.blit(pygame.image.load(mp.mach_coor(zoom, posi=pos, mash=mash, place=place)), (0, 0))
-    pygame.draw.rect(screen, color[0], (8, 8, 50, 20))
+    pygame.draw.rect(screen, color[0], (8, 50, 50, 20))
     font_type = pygame.font.Font(None, 17)
     text = font_type.render('схема', True, (255, 255, 255))
-    screen.blit(text, (10, 11))
-    pygame.draw.rect(screen, color[1], (8, 55, 50, 20))
+    screen.blit(text, (10, 53))
+    pygame.draw.rect(screen, color[1], (8, 90, 50, 20))
     font_type = pygame.font.Font(None, 17)
     text = font_type.render('спутник', True, (255, 255, 255))
-    screen.blit(text, (10, 57))
-    pygame.draw.rect(screen, color[2], (8, 102, 50, 20))
+    screen.blit(text, (10, 93))
+    pygame.draw.rect(screen, color[2], (8, 130, 50, 20))
     font_type = pygame.font.Font(None, 17)
     text = font_type.render('гибрид', True, (255, 255, 255))
-    screen.blit(text, (10, 105))
+    screen.blit(text, (10, 133))
     pygame.draw.rect(screen, (255, 215, 0), (311, 400, 70, 35))
     font_type = pygame.font.Font(None, 23)
     text = font_type.render('искать', True, (0, 0, 0))
@@ -207,11 +230,14 @@ while running:
     screen.blit(text, (395, 410))
     pygame.draw.rect(screen, (128, 128, 0), border_rect2)
     pygame.draw.rect(screen, (255, 255, 255), input_rect2)
-    font_type = pygame.font.Font(None, 20)
+    font_type = pygame.font.Font(None, 15)
     text = font_type.render(str(place2), True, (0, 0, 0))
-    screen.blit(text, (72, 13))
+    screen.blit(text, (10, 13))
+    pygame.draw.rect(screen, (32, 178, 170), (461, 400, 80, 35))
+    font_type = pygame.font.Font(None, 20)
+    text = font_type.render('PC/NO PC', True, (0, 0, 0))
+    screen.blit(text, (471, 410))
     pos = ''
     pygame.display.flip()
 pygame.quit()
 os.remove(mp.mach_coor(zoom))
-
